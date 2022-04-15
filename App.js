bc230b86260c5339c78ca8cb4f4ea4c837112aa9
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-04-15 14:12:08 +0700

    Add image assets

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <ImageBackground source={require("./assets/images/ModelS.jpeg")}
+      style={styles.bg}
+      />
+
+      <View style={styles.mobilContainer}>
+        <View style={styles.judul}>
+          <Text style={styles.mainJudul}>Model S</Text>
+          <Text style={styles.subJudul}>Starting at $69,420</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mobilContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  judul: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center'
+  },
+  mainJudul: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  subJudul: {
+    fontSize: 12,
+    color: 'grey'
+  },
+  bg: {
+    width: '100%',
+    height: '100%'
+  }
 });
