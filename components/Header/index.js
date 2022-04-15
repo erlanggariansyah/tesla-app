8666b2e9f60ba2819c006a97299e3a01bee9476b
Erlangga Riansyah <erlanggariansyah0@gmail.com>
2022-04-15 21:27:03 +0700

    Touchable tesla logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { View, Image, Linking, onPress, TouchableOpacity } from "react-native";
 import styles from "./style";
 
 const Header = function () {
     return (
         <View style={styles.container}>
-            <Image style={styles.image} source={require('../../assets/images/logo.png')} />
+            <TouchableOpacity onPress={() => {Linking.openURL("https://tesla.com")}}>
+                <Image style={styles.image} 
+                source={require('../../assets/images/logo.png')} 
+                />
+            </TouchableOpacity>
             <Image style={styles.menu} source={require('../../assets/images/menu.png')} />
         </View>
     );
